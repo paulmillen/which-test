af5c6a7e746d17237ee428e08a9310f4bdb8512c
unknown <pwmillen@gmail.com>
2023-02-01 09:01:19 +0000

    adjust react root injection

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -9,15 +9,17 @@ import './index.css';
 
 const queryClient = new QueryClient()
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement
+const root = ReactDOM.createRoot(container)
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-          <App />
+        <App />
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
